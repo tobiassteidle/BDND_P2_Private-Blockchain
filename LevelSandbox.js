3ad7de767f74b1a32fc28063224ca29bdd0f3a8b
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-12-20 11:15:39 +0100

    feat: implement methods for LevelDB persistence

--- LevelSandbox.js
+++ LevelSandbox.js
@@ -16,6 +16,15 @@ class LevelSandbox {
         let self = this;
         return new Promise(function(resolve, reject) {
             // Add your code here, remember un Promises you need to resolve() or reject()
+            self.db.get(key, function(err, value) {
+                if(err) {
+                    console.log('Not found!', err);
+                    reject(err);
+                } else {
+                    console.log('Value = ' + value);
+                    resolve(value);
+                }
+            })
         });
     }
 
@@ -23,19 +32,35 @@ class LevelSandbox {
     addLevelDBData(key, value) {
         let self = this;
         return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject() 
+            // Add your code here, remember un Promises you need to resolve() or reject()
+            self.db.put(key, value, function(err) {
+                if (err) {
+                    console.log('Block ' + key + ' submission failed', err);
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            })
         });
     }
 
     // Method that return the height
     getBlocksCount() {
+        // Add your code here, remember un Promises you need to resolve() or reject()
         let self = this;
         return new Promise(function(resolve, reject){
-            // Add your code here, remember un Promises you need to resolve() or reject()
+            let i = 0;
+            self.db.createReadStream().on('data', function(data) {
+                i++;
+            }).on('error', function(err) {
+                console.log('Unable to read data stream!', err)
+                reject(err);
+            }).on('close', function() {
+                console.log('Block Count ' + i);
+                resolve(i)
+            });
         });
     }
-        
-
 }
 
 module.exports.LevelSandbox = LevelSandbox;
